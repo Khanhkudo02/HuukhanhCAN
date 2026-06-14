16ecc22c0af8b3bb3ea6f1a1819e6748462058db
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-06-14 22:12:21 +0700

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,29 +1,37 @@
 const subjects = [
-
-    "Tiếng Anh",
-    "Pháp luật đại cương",
-    "Triết học Mác - Lênin",
-    "Tâm lý học",
-    "Tin học",
-    "An ninh mạng",
-    "Điều tra hình sự"
-
+{
+name: “Tâm lý học”,
+file: “subjects/tamlyhoc.html”
+},
+{
+name: “Tiếng Anh”,
+file: “subjects/tienganh.html”
+},
+{
+name: “Pháp luật đại cương”,
+file: “subjects/phapluat.html”
+}
 ];
 
-function renderSubjects(){
+function renderSubjects() {
+const list = document.getElementById(“subjectList”);
 
-    const list = document.getElementById("subjectList");
+list.innerHTML = "";
+subjects.forEach(subject => {
+    list.innerHTML += `
+        <li onclick="loadSubject('${subject.file}')">
+            ${subject.name}
+        </li>
+    `;
+});
 
-    list.innerHTML = "";
+}
 
-    subjects.forEach(subject => {
+async function loadSubject(file) {
+const response = await fetch(file);
+const html = await response.text();
 
-        const li = document.createElement("li");
-
-        li.textContent = subject;
-
-        list.appendChild(li);
-
-    });
+document.getElementById("content").innerHTML = html;
+closeSidebar();
 
 }
