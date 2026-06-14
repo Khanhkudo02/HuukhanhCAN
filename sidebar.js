6eba774bda7536dc8755a58c701f2f79b20c8d22
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-06-14 22:16:57 +0700

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,37 +1,37 @@
 const subjects = [
-{
-name: “Tâm lý học”,
-file: “subjects/tamlyhoc.html”
-},
-{
-name: “Tiếng Anh”,
-file: “subjects/tienganh.html”
-},
-{
-name: “Pháp luật đại cương”,
-file: “subjects/phapluat.html”
-}
+    {
+        name: "Tâm lý học",
+        file: "subjects/tamlyhoc.html"
+    },
+    {
+        name: "Tiếng Anh",
+        file: "subjects/tienganh.html"
+    },
+    {
+        name: "Pháp luật đại cương",
+        file: "subjects/phapluat.html"
+    }
 ];
 
 function renderSubjects() {
-const list = document.getElementById(“subjectList”);
+    const list = document.getElementById("subjectList");
 
-list.innerHTML = "";
-subjects.forEach(subject => {
-    list.innerHTML += `
-        <li onclick="loadSubject('${subject.file}')">
-            ${subject.name}
-        </li>
-    `;
-});
+    list.innerHTML = "";
 
+    subjects.forEach(subject => {
+        list.innerHTML += `
+            <li onclick="loadSubject('${subject.file}')">
+                ${subject.name}
+            </li>
+        `;
+    });
 }
 
 async function loadSubject(file) {
-const response = await fetch(file);
-const html = await response.text();
+    const response = await fetch(file);
+    const html = await response.text();
 
-document.getElementById("content").innerHTML = html;
-closeSidebar();
+    document.getElementById("content").innerHTML = html;
 
+    closeSidebar();
 }
